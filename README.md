24e26e5055a841be71d8188772335002d866f549
Boris Stelmakh <mikechip@pm.me>
2021-12-31 16:54:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# BanTest
+
+Just a simple Vue frontend. Needed to test the availability of the domain.
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main class="flex-shrink-0">
+      <div class="container">
+        <h1 class="mt-5">Похоже, всё в порядке</h1>
+        <p class="lead">Если вы это читаете, значит пока пронесло. Но кто знает, что будет дальше?</p>
+      </div>
+    </main>
+
+    <footer class="footer mt-auto py-3 bg-light">
+      <div class="container">
+        <span class="text-muted">
+          Посвящается любителям что-нибудь заблокировать
+        </span>
+        <b>|</b>&nbsp;
+        <a href="https://github.com/kt-soft-dev/ban-test">GitHub</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
